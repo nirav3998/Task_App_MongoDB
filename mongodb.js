bf44618d8eeb6260a6b39ea85fde743202723127
Nirav Mendapara <nirav3998@gmail.com>
2023-01-01 13:48:11 +0530

    Console log message Updation

--- mongodb.js
+++ mongodb.js
@@ -8,12 +8,12 @@ const mongoClient = mongodb.MongoClient
 const clientUrl = 'mongodb://127.0.0.1:27017'
 const dataBaseName = 'task-manager'
 
-//Connecting with the server
+//Connecting with the server and also we will be used for Inserting the data into Database
 
 mongoClient.connect(clientUrl,{useNewUrlParser:true},(error,client)=>{
 
     if (error){
-        return console.log('Unable to connect with the database!!!')
+        return console.log('Unable to connect with the database, Please try again!!!')
     }
 
     const db = client.db(dataBaseName)
